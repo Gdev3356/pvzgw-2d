e9950bceaad5c884e3b62dc92bb880962d885237
= <=>
2026-09-19 22:39:41 -0300

    feat: Rebalancing the Peashooter

--- shared/src/engine.ts
+++ shared/src/engine.ts
@@ -27,7 +27,7 @@ export const CLASS_CONFIGS: Record<CharacterClass, ClassConfig> = {
         weapon: {
             name: 'Pea Cannon',
             maxAmmo: 10,
-            fireRate: 350,
+            fireRate: 410,
             reloadDuration: 1500,
             projectileSpeed: 10,
             directDamage: 25,
